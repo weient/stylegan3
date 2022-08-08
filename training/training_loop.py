d21a9024cecda82a6e320127d8f9478a2c25e613
weient <weien@gapp.nthu.edu.tw>
2022-08-08 21:36:17 +0800

    change dir

--- training/training_loop.py
+++ training/training_loop.py
@@ -17,14 +17,14 @@ import psutil
 import PIL.Image
 import numpy as np
 import torch
-import dnnlib
-from torch_utils import misc
-from torch_utils import training_stats
-from torch_utils.ops import conv2d_gradfix
-from torch_utils.ops import grid_sample_gradfix
+from .. import dnnlib
+from ..torch_utils import misc
+from ..torch_utils import training_stats
+from ..torch_utils.ops import conv2d_gradfix
+from ..torch_utils.ops import grid_sample_gradfix
 
 import legacy
-from metrics import metric_main
+from ..metrics import metric_main
 
 #----------------------------------------------------------------------------
 
