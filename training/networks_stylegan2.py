8e29029e0f89cae923cc5c0524c62efb90afdd1f
weient <weien@gapp.nthu.edu.tw>
2022-09-11 23:29:14 +0800

    ws dim

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -588,9 +588,9 @@ class SynthesisNetwork(torch.nn.Module):
             is_last = (res == self.img_resolution)
             block = SynthesisBlock(in_channels, out_channels, w_dim=w_dim, resolution=res,
                 img_channels=img_channels, is_last=is_last, use_fp16=use_fp16, **block_kwargs)
-            self.num_ws += block.num_conv
-            if is_last:
-                self.num_ws += block.num_torgb
+            self.num_ws += (block.num_conv + block.num_torgb)
+            #if is_last:
+            #    self.num_ws += block.num_torgb
             setattr(self, f'b{res}', block)
 
     def forward(self, encoder_out, ws, **block_kwargs):
