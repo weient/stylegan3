9609ee3537678085b6d6a870c23d142f54858678
weient <weien@gapp.nthu.edu.tw>
2022-10-16 22:04:36 +0800

    phase word

--- training/training_loop.py
+++ training/training_loop.py
@@ -320,7 +320,7 @@ def training_loop(
             phase_real_text = (phase_real_text.to(device).to(torch.float32) / 127.5 - 1).split(batch_gpu)
             phase_real_rec = (phase_real_rec.to(device).to(torch.float32) / 127.5 - 1).split(batch_gpu)
             phase_box = (phase_box.to(device).to(torch.float32)).split(batch_gpu)
-            phase_word = (phase_word).split(batch_gpu)
+            #phase_word = (phase_word).split(batch_gpu)
 
             all_gen_z = torch.randn([len(phases) * batch_size, G.z_dim], device=device)
             all_gen_z = [phase_gen_z.split(batch_gpu) for phase_gen_z in all_gen_z.split(batch_size)]
@@ -342,9 +342,9 @@ def training_loop(
             phase.module.requires_grad_(True)
             
             #print("phase_real_img size:", phase_real_img.size())
-            for box, real_img, real_img_rec, real_text, word, gen_z in zip(phase_box, phase_real_img, phase_real_rec, phase_real_text, phase_word, phase_gen_z):
+            for box, real_img, real_img_rec, real_text, gen_z in zip(phase_box, phase_real_img, phase_real_rec, phase_real_text, phase_gen_z):
                 #print("phase_real_img size:", phase_real_img.size())
-                loss.accumulate_gradients(bounding_box=box, phase=phase.name, real_img=real_img, real_img_rec=real_img_rec, real_text = real_text, word_label = word, real_c=None, gen_z=gen_z, gen_c=None, gain=phase.interval, cur_nimg=cur_nimg)
+                loss.accumulate_gradients(bounding_box=box, phase=phase.name, real_img=real_img, real_img_rec=real_img_rec, real_text = real_text, word_label = phase_word, real_c=None, gen_z=gen_z, gen_c=None, gain=phase.interval, cur_nimg=cur_nimg)
             phase.module.requires_grad_(False)
 
             # Update weights.
