752fad15def17e1bad93abbbed04875fb5a4f905
weient <weien@gapp.nthu.edu.tw>
2022-10-22 22:14:31 +0800

    resne

--- training/resnet.py
+++ training/resnet.py
@@ -84,7 +84,7 @@ class SaveLayer(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=2):
+    def __init__(self, block, num_blocks, num_classes=2645):
         super(ResNet, self).__init__()
         self.in_planes = 64
         self.save_layers = SaveLayer()
